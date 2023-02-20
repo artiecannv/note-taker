2bd2a40ebe1f84f7bd5ae2d88f6eaa8a05d764ec
artiecannv <thefiftharthur@gmail.com>
2023-02-20 12:52:47 -0700

    fixing route errors, placing them in the correct spots

--- server.js
+++ server.js
@@ -3,30 +3,48 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require("path");
 const fs = require("fs");
-const {v4:uuidv4} = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 const db = require("./db/db.json");
 
 //Parsing data from Strings, Arrays, and JSON
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//! TODO: get route for notes page (pulls from json db)
+//! TODO: get route for api/notes page (loads notes.html from public)
+//! TODO: post route for api/notes to create new note
+//! TODO: delete route, for deletion
+
+const saveFunction = (notes) => {
+  fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+};
+
+//! TODO: get route for home page
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}. Welcome!`);
-  });
-
-  //! TODO: get route for home page
-  //! TODO: get route for notes page (pulls from json db)
-  //! TODO: get route for api/notes page (loads notes.html from public)
-  //! TODO: post route for api/notes to create new note
-  //! TODO: delete route, for deletion
-
- const saveFunction = (notes) => {
-  fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-    if (err) {
-      console.error(err)
-    } 
-  });
- }; 
+  console.log(`API server now on port ${PORT}. Welcome!`);
+});
