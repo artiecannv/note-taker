11de28fdb3e624652ea0863a34d0b8f523309912
artiecannv <thefiftharthur@gmail.com>
2023-02-20 12:34:37 -0700

    imported packages in server.js file

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
-## Note Taker Application
+# Note Taker Application
 
-#
+
+## Description
+
+
+
+## Installation
+
+
+
+## Usage
+
+Add the video link and a screenshot of the original page
+
+
+## Credits 
+
+Worked with Christin Carter, a fellow classmate of mine
+
+## License
+
+N/A
+
+## Tests
+
+
+
+## Contact for Questions
+
+Email me at: thefiftharthur@gmail.com
+GitHub Profile: https://github.com/artiecannv
+
+## GitHub Repository
+
+https://github.com/artiecannv/note-taker
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const path = require("path");
+const fs = require("fs");
+const {v4:uuidv4} = require("uuid");
+const db = require("./db/db.json");
 
 //Parsing data from Strings, Arrays, and JSON
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}. Welcome!`);
