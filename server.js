2025a0d013d6763876d0f4fe628da96ba00a645d
artiecannv <thefiftharthur@gmail.com>
2023-02-20 13:56:48 -0700

    website functioning properly, testing completed

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//* TODO: save function for any changes
-
+// Save function for any changes
 const saveFunction = (notes) => {
   fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
     if (err) {
@@ -21,24 +20,23 @@ const saveFunction = (notes) => {
   });
 };
 
-//* TODO: get route for home page
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+//Get route for home page
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-//* TODO: get route for notes page (pulls from json db)
+//Get route for notes page (pulls from json db)
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-//* TODO: get route for api/notes page (loads notes.html from public)
-app.get("/notes", (req, res) => {
-  let currentNotes = notes;
-  res.json(currentNotes);
+//Get route for api/notes page (loads notes.html from public)
+app.get("/api/notes", (req, res) => {
+  res.json(db);
 });
 
-//* TODO: post route for api/notes to create new note
-app.post("/notes", (req, res) => {
+//Post route for api/notes to create new note
+app.post("/api/notes", (req, res) => {
   req.body.id = uuidv4();
   const newNote = {
     id: req.body.id,
@@ -51,14 +49,13 @@ app.post("/notes", (req, res) => {
   res.status(200).json(newNote);
 });
 
-//* TODO: delete route, for deletion
-
-app.delete("/notes/:id", (req, res) => {
+//Delete route, for deletion
+app.delete("/api/notes/:id", (req, res) => {
   const noteIndex = db.findIndex((currentIndex) => {
     return currentIndex.id === req.params.id;
   });
   const tempDb = db;
-  tempDb.splice(currentIndex, 1);
+  tempDb.splice(noteIndex, 1);
   saveFunction(tempDb);
   return res.json(`${req.params.id} has been deleted`);
 });
